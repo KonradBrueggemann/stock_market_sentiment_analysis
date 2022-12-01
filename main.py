11bb469e7bfe89e2b823978d0be0313c8e2489ab
Konrad Brüggemann <konrad.brueggemann@uni-potsdam.de>
2022-12-01 13:21:58 +0100

    added runnable main.py and changed structure

---
+++ main.py
@@ -0,0 +1,13 @@
+from dataframe_generator import DataFrame
+
+
+def main(q, after, before):
+    df = DataFrame(q, after, before)
+    print(df.convert_to_pandas_df())
+
+
+if __name__ == "__main__":
+    query = input("Input Stock Ticker: ")
+    start = input("Start Date: ")
+    end = input("End Date: ")
+    main(query, start, end)
