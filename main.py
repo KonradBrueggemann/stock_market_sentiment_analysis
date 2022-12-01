b5b390781cf806388e3f6d178bd8eff01dd1071e
Konrad Brüggemann <konrad.brueggemann@uni-potsdam.de>
2022-12-01 14:00:14 +0100

    added multiprocessing

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from dataframes.dataframe_generator import DataFrame
+from threading import Thread
+from multiprocessing import Process
+
+import time
 
 
 def main(q, after, before):
@@ -8,7 +12,13 @@ def main(q, after, before):
 
 
 if __name__ == "__main__":
-    query = input("Input Stock Ticker: ")
-    start = input("Start Date: ")
-    end = input("End Date: ")
-    main(query, start, end)
+    stocks = ["NFLX", "TSLA"]
+    start = "01-11-2022"
+    end = "07-11-2022"
+
+    threads = [Process(target=main, args=(stock, start, end)) for stock in stocks]
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+    print("Done.")
