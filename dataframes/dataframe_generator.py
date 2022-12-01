a7b926550328a7e76a462c553601e2670189e32f
Konrad Brüggemann <konrad.brueggemann@uni-potsdam.de>
2022-12-01 18:04:26 +0100

    fixed time frame date list to work for several months

--- dataframes/dataframe_generator.py
+++ dataframes/dataframe_generator.py
@@ -1,4 +1,5 @@
 from dataframes.scores_for_source import ScoreChart
+from datetime import date, timedelta
 
 import pandas as pd
 import time
@@ -15,21 +16,22 @@ class DataFrame:
 
     def list_of_dates(self):
         """ generates a list of dates between two dates """
-        date_list = []
 
         start_chops = self.start.split("-")
         month = int(start_chops[1])   # the input is a string like "12-05-2022"
-        year = int(start_chops[2])    # so the first slice is the day, which will be gradually increased
-        start = int(start_chops[0])
+        year = int(start_chops[2])    # so the first slice is the day and so on
+        day = int(start_chops[0])
+        sdate = date(year, month, day)
 
         end_chops = self.end.split("-")
-        end = int(end_chops[0])
+        month = int(end_chops[1])
+        year = int(end_chops[2])
+        day = int(end_chops[0])
+        edate = date(year, month, day)
 
-        # this currently only works within the same month
-        # so its best to use it for time frames that go from first of month to end of month
-        for i in range(start, end + 1):
-            date = f"{i}-{month}-{year}"
-            date_list.append(date)
+        timestamps = pd.date_range(sdate, edate-timedelta(days=1), freq='d').to_list()
+        date_list = [str(stamp.date().strftime("%d-%m-%Y")) for stamp in timestamps]
+        print(date_list)
 
         return date_list
 
@@ -38,17 +40,18 @@ class DataFrame:
         return score.get_polarity_score()
 
     @staticmethod
-    def calc_day_after(date):
+    def calc_day_after(sdate):
         """
         simple method to increase the day in a D-M-Y date string by 1
         doesnt account for months (yet)
         """
-        date_chops = date.split("-")
+        date_chops = str(sdate).split("-")
         month = int(date_chops[1])
         year = int(date_chops[2])
         day = int(date_chops[0])
-        res = f"{day+1}-{month}-{year}"
-        return res
+        current_date = date(year, month, day)
+        next_day = current_date + timedelta(days=1)
+        return next_day.strftime("%d-%m-%Y")
 
     def run_through_datelist(self):
         """ iterates through the date list to get the SP score for each day """
@@ -56,7 +59,7 @@ class DataFrame:
         for start_date in self.dates:
             end_date = self.calc_day_after(start_date)   # to get the comments of day X, do X-M-Y to (X+1)-M-Y
             date_score_dict[start_date] = self.get_sentiment_score(start_date, end_date)
-            time.sleep(1)   # this makes sure the pushshift.io API is ready for the next request
+            time.sleep(1.5)   # this makes sure the pushshift.io API is ready for the next request
         return date_score_dict
 
     def convert_to_pandas_df(self):
