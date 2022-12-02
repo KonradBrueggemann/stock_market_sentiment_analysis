8fdb48dc5118135d2be87e83647b9abfafedcbe7
Konrad Brüggemann <konrad.brueggemann@uni-potsdam.de>
2022-12-02 17:35:56 +0100

    now using ML model for sentiment analysis

--- dataframes/text_processing/sentiment_analysis.py
+++ dataframes/text_processing/sentiment_analysis.py
@@ -1,11 +1,16 @@
 import nltk
 from nltk import word_tokenize
 from nltk.corpus import stopwords
+import nltk.sentiment.vader as sev
+
 from string import punctuation
 
-import nltk.sentiment.vader as sev
 import numpy as np
 import pandas as pd
+
+from transformers import AutoTokenizer
+from transformers import AutoModelForSequenceClassification
+from scipy.special import softmax
 
 
 class SentimentAnalysis:
@@ -13,22 +18,22 @@ class SentimentAnalysis:
         self.comments = comments   # these will be passed in list format from the main class
         self.volume = self.get_post_volume()
         self.senti_table = pd.read_csv("resources/Loughran-McDonald_MasterDictionary_1993-2021.csv")
+        self.sia = sev.SentimentIntensityAnalyzer()
+        self.model_name = f"cardiffnlp/twitter-roberta-base-sentiment"
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+        self.model = AutoModelForSequenceClassification.from_pretrained(self.model_name)
 
     def return_polarity_score(self, token):
-        # get index of row where word column value is the token
-        index = self.senti_table.index[self.senti_table['Word'] == token.upper()].to_list()
-        if not index:   # if there is no such value, return 0 (neutral score)
-            return 0
-        else:
-            index = index[0]  # index is a list with one element
-        negative = self.senti_table.at[index, "Negative"]
-        positive = self.senti_table.at[index, "Positive"]
-        if negative == 0 and positive != 0:
-            return 1
-        elif negative != 0 and positive == 0:
-            return -1
-        elif negative == 0 and positive == 0:
-            return 0
+        encoded_text = self.tokenizer(token, return_tensors="pt")
+        output = self.model(**encoded_text)
+        scores = output[0][0].detach().numpy()
+        scores = softmax(scores)
+        scores_dict = {
+            "neg": scores[0],
+            "neu": scores[1],
+            "pos": scores[2]
+        }
+        return scores_dict["pos"]
 
     def tokenize(self, comment):
         stop = stopwords.words()
@@ -36,16 +41,23 @@ class SentimentAnalysis:
         tokens = [token for token in tokens if token not in stop and token not in punctuation]
         return tokens
 
-    def get_post_tag(self, token):
+    def sentenize(self, comment):
+        return nltk.sent_tokenize(comment)
+
+    def get_pos_tag(self, token):
         tup = nltk.pos_tag([token])
         return tup[0][1]
 
-    def get_mean_score(self):
+    def _get_mean_score(self):
         total = []
         for comment in self.comments:
-            tokens = self.tokenize(comment)
-            for token in tokens:
-                total.append(self.return_polarity_score(token))
+            sentences = self.sentenize(comment)
+            for sentence in sentences:
+                total.append(self.return_polarity_score(sentence))
+        return total
+
+    def get_mean_score(self):
+        total = self._get_mean_score()
         return float(np.round(np.mean(total), 5))   # get mean score for list of comments
 
     def get_post_volume(self):
