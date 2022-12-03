b6d6064b4616f267adc524832423734b7b45e0d6
Konrad Brüggemann <brueggemann4@uni-potsdam.de>
2022-12-03 12:09:13 +0100

    now uses Nuno Oliveira's stock market sentiment lexicon

--- dataframes/text_processing/sentiment_analysis.py
+++ dataframes/text_processing/sentiment_analysis.py
@@ -4,52 +4,76 @@ from nltk import word_tokenize
 from nltk.corpus import stopwords
 import nltk.sentiment.vader as sev
 
-# for removing punctuations from the tokens
-from string import punctuation
-
 # math
 import numpy as np
 import pandas as pd
 
-# machine learning based approach
-from transformers import AutoTokenizer
-from transformers import AutoModelForSequenceClassification
-from scipy.special import softmax
-
-import torch
-
 
 class SentimentAnalysis:
     def __init__(self, comments):
-        self.comments = comments   # these will be passed in list format from the main class
+        self.comments = comments  # these will be passed in list format from the main class
         self.volume = self.get_post_volume()
-        self.senti_table = pd.read_csv("resources/Loughran-McDonald_MasterDictionary_1993-2021.csv")
+        self.master_dict = pd.read_csv("resources/Loughran-McDonald_MasterDictionary_1993-2021.csv")
+        self.stocklex = pd.read_csv("resources/Nuno_Oliveira_Stock_Lexicon.csv",
+                                    sep=",",
+                                    decimal=".")
+        self.stock_symbols = pd.read_csv("resources/nasdaq_screener_1667058131530.csv")["Symbol"].values
         self.sia = sev.SentimentIntensityAnalyzer()
-        self.model_name = f"cardiffnlp/twitter-roberta-base-sentiment"
-        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
-        self.model = AutoModelForSequenceClassification.from_pretrained(self.model_name).to(self.device)
 
-    def return_polarity_score(self, token, type):
-        if type == "roberta":
-            encoded_text = self.tokenizer(token, return_tensors="pt")
-            output = self.model(**encoded_text)
-            scores = output[0][0].detach().numpy()
-            scores = softmax(scores)
-            scores_dict = {
-                "neg": scores[0],
-                "neu": scores[1],
-                "pos": scores[2]
-            }
-            return scores_dict["pos"]
+    def return_polarity_score(self, sentence, type):
+        if type == "nltk":
+            return self.sia.polarity_scores(sentence)["compound"]
+        elif type == "stocklex":
+            return self.get_stock_lex_val(sentence)
 
-        elif type == "nltk":
-            return self.sia.polarity_scores(token)["compound"]
+    def get_stock_lex_val(self, sentence):
+        values = []
+        data = self.stocklex
+        tokens = self.tokenize(sentence)
+        tokens = self.apply_tags(tokens)
+        bigrams = self._bigrams(tokens)
+        ignore = []
+        for bigram in bigrams:
+            if bigram in data['Item'].values:
+                score = data.loc[data['Item'] == bigram, 'Aff_Score'].item()
+                values.append(score)
+                ignore.append(bigram)
+
+        for token in tokens:
+            if token not in '\t'.join(ignore) and token not in stopwords.words():
+                if token in data['Item'].values:
+                    try:
+                        score = data.loc[data['Item'] == token, 'Aff_Score'].item()
+                        values.append(score)
+                        ignore.append(token)
+                    except ValueError:
+                        values.append(self.sia.polarity_scores(token)["compound"])
+                else:
+                    values.append(self.sia.polarity_scores(token)["compound"])
+
+        return np.mean(values) if values != [] else 0
+
+    def apply_tags(self, tokens):
+        # the stock market sentiment lexicon uses tags for certain types of items
+        for i, token in enumerate(tokens):
+            if token.isnumeric():
+                tokens[i] = "NUM"
+            if token.upper() in self.stock_symbols:
+                tokens[i] = "tkr"
+        return tokens
+
+    def _bigrams(self, tokens):
+        """ creates list of bigrams for a sentence """
+        list_of_bigrams = []
+        for i, token in enumerate(tokens):
+            if i < len(tokens)-1:
+                bigram_lst = [token, tokens[i + 1]]
+                bigram = " ".join(bigram_lst)
+                list_of_bigrams.append(bigram)
+        return list_of_bigrams
 
     def tokenize(self, comment):
-        stop = stopwords.words()
         tokens = word_tokenize(comment)
-        tokens = [token for token in tokens if token not in stop and token not in punctuation]
         return tokens
 
     def sentenize(self, comment):
@@ -64,13 +88,13 @@ class SentimentAnalysis:
         for comment in self.comments:
             sentences = self.sentenize(comment)
             for sentence in sentences:
-                total.append(self.return_polarity_score(sentence, type="nltk"))
+                total.append(self.return_polarity_score(sentence, type="stocklex"))
         return total
 
     def get_mean_score(self):
         total = self._get_mean_score()
         result = float(np.round(np.mean(total), 5))
-        return result if self.volume > 0 else 0   # get mean score for list of comments, if no comments return 0
+        return result if self.volume > 0 else 0  # get mean score for list of comments, if no comments return 0
 
     def get_post_volume(self):
         return len(self.comments)
