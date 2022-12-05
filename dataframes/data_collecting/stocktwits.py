2d3e582111046f48b88d95d6faaf8426333d075f
Konrad Brüggemann <brueggemann4@uni-potsdam.de>
2022-12-05 12:59:44 +0100

    get twits for specific date

--- dataframes/data_collecting/stocktwits.py
+++ dataframes/data_collecting/stocktwits.py
@@ -3,13 +3,19 @@ from dataframes.data_collecting.auxilliary import twit_format
 
 
 class Stocktwits:
-    def __init__(self, query, start):
+    def __init__(self, query, start, end):
         self.query = query
         self.start = twit_format(start)
+        self.end = twit_format(end)
         self.twits = self.get_tweets()
 
     def get_tweets(self):
         twits = Collector()
-        messages = twits.get_history({'symbols': [self.query], 'start': self.start})  # messages have a lot of attrs
-        comments = [message["body"] for message in messages]   # so we extract only the body (text content)
+        messages = twits.get_history({'symbols': [self.query], 'start': self.start}) # messages have a lot of attrs
+        comments = [message["body"] for message in messages if message["created_at"] == str(self.start).partition("T")[0]] # so we extract only the body (text content)
         return comments
+
+
+if __name__ == "__main__":
+    twits = Stocktwits("NFLX", "03-12-2022", "04-12-2022")
+    print(twits.twits[:3])
--- dataframes/scores_for_source.py
+++ dataframes/scores_for_source.py
@@ -11,38 +11,45 @@ class ScoreChart:
     def __init__(self, query, after, before, sources):
         self.query = query
         self.start = after   # stocktwits scraper doesnt need the unix timestamp
+        self.end = before
         self.after = unix_timestamp(after)   # takes date in D-M-Y format and converts it to unix timestamp
         self.before = unix_timestamp(before)
         self.sources = sources
         self.comments = self.get_comments()
-        self.SA = SentimentAnalysis(self.comments[:20])
+        self.reddit_sentiment = SentimentAnalysis(self.comments["reddit"][:20])
+        self.stocktwits_sentiment = SentimentAnalysis(self.comments["stocktwits"][:100])
         self.AV = Visualizer(after, before, query)
-        self.sentiment = self.get_polarity_score()
+        self.rsent = self.get_polarity_score_reddit()
+        self.tsent = self.get_polarity_score_stocktwits()
 
     def get_comments(self):
         """
         uses RedditComments class to make a request to the pushshift.io reddit archive
         a list of comments will be returned
         """
-        comments = []
+        comments = {}
 
         if "reddit" in self.sources:
             reddit = RedditComments(self.query, self.after, self.before)
-            comments.extend(reddit.comments)
+            comments["reddit"] = reddit.comments
 
         if "stocktwits" in self.sources:
-            stocktwits = Stocktwits(self.query, self.start)
-            comments.extend(stocktwits.twits)
+            stocktwits = Stocktwits(self.query, self.start, self.end)
+            comments["stocktwits"] = stocktwits.twits
 
         return comments
 
-    def get_polarity_score(self):
+    def get_polarity_score_reddit(self):
         """ calls SentimentAnalysis class to get the mean sentiment polarity score for the comment list """
-        return self.SA.get_mean_score()
+        return self.reddit_sentiment.get_mean_score()
+
+    def get_polarity_score_stocktwits(self):
+        """ calls SentimentAnalysis class to get the mean sentiment polarity score for the comment list """
+        return self.stocktwits_sentiment.get_mean_score()
 
     def get_volume(self):
         """ calls SentimentAnalysis class to get the comment volume"""
-        return len(self.comments)
+        return len(self.comments["reddit"]) + len(self.comments["stocktwits"])
 
     def _get_last_business_day(self, data, date):
         if vantage_date(date) in data['date'].values:
