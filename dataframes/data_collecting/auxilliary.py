f61ac636f836a60bb0885df1529a867fc401c5b5
Konrad Brüggemann <brueggemann4@uni-potsdam.de>
2022-12-07 22:37:08 +0100

    removed useless dictionary

--- dataframes/data_collecting/auxilliary.py
+++ dataframes/data_collecting/auxilliary.py
@@ -2,21 +2,6 @@ import datetime
 import time
 from datetime import date, timedelta
 
-month_dict = {
-    "Jan": "01",
-    "Feb": "02",
-    "Mar": "03",
-    "Apr": "04",
-    "May": "05",
-    "Jun": "06",
-    "Jul": "07",
-    "Aug": "08",
-    "Sep": "09",
-    "Oct": "10",
-    "Nov": "11",
-    "Dec": "12"
-}
-
 
 def tweet_to_dmy(date: str):
     toks = str(date.split(" ")[0]).split("-")
