e11b5e9c5cf9a2bc37417310632ab21b89b5bf1f
Konrad Brüggemann <brueggemann4@uni-potsdam.de>
2022-12-08 01:28:19 +0100

    fine tuning the model

--- dataframes/text_processing/classifier.py
+++ dataframes/text_processing/classifier.py
@@ -12,10 +12,13 @@ from nltk.corpus import stopwords
 class Classifier:
     def __init__(self):
         pd.set_option('max_colwidth', 1000)
-        self.df = pd.read_csv("twitter_2013_labelled_tweets.csv")
-        self.vectorizer = CountVectorizer(max_features=2000, min_df=5, max_df=0.7, stop_words=stopwords.words())
+        self.df = pd.read_csv("resources/twitter_2013_labelled_tweets.csv")
+        self.vectorizer = CountVectorizer(max_features=1500, min_df=0.0009, max_df=0.9)
         self.tfidfconverter = TfidfTransformer()
         self.model = self.initialize_model()
+        self.classifier = self.model[0]
+        self.X = self.model[1]
+        self.y = self.model[2]
 
     def initialize_model(self):
         X, y = self.df["text"], self.df["sentiment"]
@@ -26,13 +29,20 @@ class Classifier:
         model = MultinomialNB()
         model.fit(X, y)
 
-        return model
+        return model, X, y
+
+    def test_model(self):
+        X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, test_size=0.2, random_state=42)
+        y_pred = self.classifier.predict(X_test)
+        return classification_report(y_test, y_pred)
 
     def classify(self, tweet):
         sample = self.vectorizer.transform([tweet])
-        return self.model.predict(sample)[0]
+        return self.classifier.predict(sample)[0]
 
 
 if __name__ == "__main__":
     c = Classifier()
-    print(c.classify(str(input("Tweet: "))))
+    print(c.test_model())
+    while True:
+        print(c.classify(str(input("Tweet: "))))
